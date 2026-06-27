2c1897a6e24767eb719ea4a2693189dc9c1070b3
alibras257 <alibras257@gmail.com>
2026-06-27 21:09:57 +0100

    Updated Dockerfile, app, CI-CD pipeline and docker-compose

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "DevOps project is running 🚀"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
